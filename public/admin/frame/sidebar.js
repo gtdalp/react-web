c6f297b7d9afd3e424a3d0a14fc3d4a1fd7add35
Microlv <andy.lv@live.com>
2015-09-29 18:44:19 +0800

    sidebar

--- public/admin/frame/sidebar.js
+++ public/admin/frame/sidebar.js
@@ -3,7 +3,20 @@
 
   var SideBarSelector = React.createClass({
     render: function () {
-      return <div>hello</div>
+      return <aside className="main-sidebar">
+        <section className="sidebar">
+          <div className="user-panel">
+            <div className="pull-left image">
+              <img src="../lib/AdminLTE/dist/img/user2-160x160.jpg" className="img-circle" alt="User Image"/>
+            </div>
+            <div className="pull-left info">
+              <p>Alexander Pierce</p>
+              <a href="#"><i className="fa fa-circle text-success"></i> Online</a>
+            </div>
+          </div>
+          <div id="navigator"></div>
+        </section>
+      </aside>;
     }
   });
 
