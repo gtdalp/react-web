ad16fd9f9f5ad7104362f86f984509cfb0ea707f
Microlv <andy.lv@live.com>
2015-09-29 16:26:08 +0800

    frame js

--- model/index.js
+++ model/index.js
@@ -2,11 +2,26 @@
 
 var mongoose = require('mongoose');
 var config = require('../config');
-var user = require('./user');
+//var user = require('./user');
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var mySchema = Schema({name: String});
+
+var User = mongoose.model('User', mySchema);
+
 
 mongoose.connect(config.dbconnect, function (err) {
   if (err) throw err;
+  User.find({name: '2'})
+    .exec(function (err, docs) {
+      if (err) return done(err);
+
+      console.log(err);
+      console.log(docs);
+    });
 });
+
 
 module.exports = {
   User: mongoose.model('User')
--- model/user.js
+++ model/user.js
@@ -4,5 +4,4 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var mySchema = Schema({ name: String });
 
-/* global db */
 module.exports = mongoose.model('User', mySchema);
