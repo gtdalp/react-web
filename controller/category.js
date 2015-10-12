c710c6ca2da26c9e0e08e81cd65a20f08bffa050
microlv <andy.lv@live.com>
2015-10-12 23:54:42 +0800

    make login to index

--- controller/category.js
+++ controller/category.js
@@ -15,18 +15,18 @@ function getCategory(req, res) {
   });
 }
 
-//function groupCategory(req, res) {
-//  dao.getCategory(function (err, data) {
-//    var obj = {code: '10086', msg: err};
-//    if (data) {
-//      obj.code = '8000';
-//      obj.data = data;
-//      obj.msg = 'sucess';
-//    }
-//
-//    res.send(obj);
-//  });
-//}
+function groupCategory(req, res) {
+  dao.getCategory(function (err, data) {
+    var obj = {code: '10086', msg: err};
+    if (data) {
+      obj.code = '8000';
+      obj.data = data;
+      obj.msg = 'sucess';
+    }
+
+    res.send(obj);
+  });
+}
 
 module.exports = {
   getCategory: getCategory,
--- routes/html.api.js
+++ routes/html.api.js
@@ -20,7 +20,7 @@ router.get('/', render('index'));
 //can't use admin,because the it conflice with public admin
 router.get('/ad', render('login'));
 router.get('/login', render('login'));
-router.get('/admin', render('admin'));
+router.get('/ad-index', render('admin'));
 
 module.exports = router;
 
