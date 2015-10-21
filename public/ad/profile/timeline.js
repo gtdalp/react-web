6aa45d1c16e9fe9dd1b7c22826382f3c05816cdd
Microlv <andy.lv@live.com>
2015-10-21 10:45:16 +0800

    time-line show

--- public/ad/profile/timeline.js
+++ public/ad/profile/timeline.js
@@ -1,51 +1,74 @@
 (function () {
   'use strict';
 
-  var colors = ['bg-red,',
-                'bg-yellow',
-                'bg-aqua',
-                'bg-blue',
-                'bg-light-blue',
-                'bg-green',
-                'bg-navy',
-                'bg-teal',
-                'bg-olive',
-                'bg-lime',
-                'bg-orange',
-                'bg-fuchsia',
-                'bg-purple',
-                'bg-maroon',
-                'bg-black'
+  var colors = [
+    'bg-red,',
+    'bg-yellow',
+    'bg-aqua',
+    'bg-blue',
+    'bg-light-blue',
+    'bg-green',
+    'bg-navy',
+    'bg-teal',
+    'bg-olive',
+    'bg-lime',
+    'bg-orange',
+    'bg-fuchsia',
+    'bg-purple',
+    'bg-maroon',
+    'bg-black'
   ];
-  var icons = ['fa-comments', 'fa-envelope', 'fa-user', 'fa-video-camera', 'fa-clock-o',
-               'fa-heart-o', 'fa-thumb-tack', 'fa-external-link', 'fa-trophy', 'fa-cogs'];
+  var icons = [
+    'fa-comments',
+    'fa-envelope',
+    'fa-user',
+    'fa-video-camera',
+    'fa-clock-o',
+    'fa-heart-o',
+    'fa-thumb-tack',
+    'fa-external-link',
+    'fa-trophy',
+    'fa-cogs'
+  ];
+  var currentDate, countDate = 0;
 
+  function dateShow(item) {
+    if (item.date !== currentDate) {
+      currentDate = item.date;
+      return <span className={round(colors)}>
+          {item.date}
+          </span>
+    }
+  }
   var TimeLineItem = React.createClass({
+    timeLabel: function (item) {
+      return <div className="time-label">
+        {dateShow(item)}
+        <i className={'fa '+ round(icons) + ' '+ round(colors)}></i>
+      </div>;
+    },
+    timeItem: function (item) {
+      return <div className="timeline-item">
+        <span className="time"><i className="fa fa-clock-o"></i>{item.time}</span>
 
+        <h3 className="timeline-header"><a href="#">{item.title}</a>{item.origin}</h3>
+
+        <div className="timeline-body">
+          {item.content}...
+        </div>
+        <div className="timeline-footer">
+          <a className="btn btn-primary btn-xs">Read more</a>
+        </div>
+      </div>;
+    },
     render: function () {
-
       var lis = this.props.items.map(function (item) {
+
         return <li>
-          <div className="time-label">
-            <span className={round(colors)}>
-              {item.date}
-            </span>
-            <i className={'fa '+ round(icons) + ' '+ round(colors)}></i>
-          </div>
-          <div className="timeline-item">
-            <span className="time"><i className="fa fa-clock-o"></i>{item.time}</span>
-
-            <h3 className="timeline-header"><a href="#">{item.title}</a>{item.origin}</h3>
-
-            <div className="timeline-body">
-              {item.content}...
-            </div>
-            <div className="timeline-footer">
-              <a className="btn btn-primary btn-xs">Read more</a>
-            </div>
-          </div>
+          {this.timeLabel(item)}
+          {this.timeItem(item)}
         </li>;
-      });
+      }.bind(this));
 
       return <ul className="timeline">
         {lis}
